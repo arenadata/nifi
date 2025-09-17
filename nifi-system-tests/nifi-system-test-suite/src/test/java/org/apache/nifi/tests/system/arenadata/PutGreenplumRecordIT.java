6f72aec34af296620841c3707c0b67eeb2ac066f
Viacheslav Vasiliev <v.vasiliev@arenadata.io>
2025-09-17 12:58:57 +0300

    ADS-2631: Add support multi-node environment in integration tests

--- nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/arenadata/PutGreenplumRecordIT.java
+++ nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/arenadata/PutGreenplumRecordIT.java
@@ -35,7 +35,11 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.shaded.org.awaitility.Awaitility;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.apache.nifi.tests.system.arenadata.util.ConfigUtil.getTestConfig;
