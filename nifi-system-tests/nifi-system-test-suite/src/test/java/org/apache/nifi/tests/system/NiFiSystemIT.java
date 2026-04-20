a2b2554a3ea2bc308d013819ca394d3794189445
Viacheslav Vasiliev <v.vasiliev@arenadata.io>
2026-04-20 16:48:28 +0300

    ADS-3259: Debug FlowSynchronizationIT

--- nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/NiFiSystemIT.java
+++ nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/NiFiSystemIT.java
@@ -405,7 +405,7 @@ public abstract class NiFiSystemIT implements NiFiInstanceProvider {
     }
 
     protected void waitFor(final ExceptionalBooleanSupplier condition) throws InterruptedException {
-        waitFor(condition, 1000L);
+        waitFor(condition, 100L);
     }
 
     protected void waitFor(final ExceptionalBooleanSupplier condition, final long delayMillis) throws InterruptedException {
