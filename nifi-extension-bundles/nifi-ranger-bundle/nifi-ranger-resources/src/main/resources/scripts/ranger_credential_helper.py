0b34c274e686fa52500ec9b40d534bb64ee315ba
Aleksey Koshelev <a.koshelev@arenadata.io>
2026-08-12 19:18:35 +0400

    ADS-3608: Migrate ranger_credential_helper.py to python3 and add a simple test for script

--- nifi-extension-bundles/nifi-ranger-bundle/nifi-ranger-resources/src/main/resources/scripts/ranger_credential_helper.py
+++ nifi-extension-bundles/nifi-ranger-bundle/nifi-ranger-resources/src/main/resources/scripts/ranger_credential_helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -14,62 +14,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import os
-from subprocess import  Popen,PIPE
-from optparse import OptionParser
+import argparse
+import getpass
+import sys
+
+from subprocess import Popen, PIPE
 
 if os.getenv('JAVA_HOME') is None:
-	print "[W] ---------- JAVA_HOME environment property not defined, using java in path. ----------"
-	JAVA_BIN='java'
+    print("[W] ---------- JAVA_HOME environment property not defined, using java in path. ----------")
+    JAVA_BIN = 'java'
 else:
-	JAVA_BIN=os.path.join(os.getenv('JAVA_HOME'),'bin','java')
-print "Using Java:" + str(JAVA_BIN)
+    JAVA_BIN = os.path.join(os.getenv('JAVA_HOME'), 'bin', 'java')
+print(f"Using Java:{JAVA_BIN}")
+
 
 def main():
+    parser = argparse.ArgumentParser(description="Manage Ranger JCEKS credential aliases.")
+    parser.add_argument("-l", "--libpath", dest="library_path",
+                        help="Path to folder where credential libs are present")
+    parser.add_argument("-f", "--file", dest="jceks_file_path", help="Path to jceks file to use")
+    parser.add_argument("-k", "--key", dest="key", help="Key to use")
+    parser.add_argument("-c", "--create", dest="create", help="Add a new alias")
+    args = parser.parse_args()
 
-	parser = OptionParser()
-
-	parser.add_option("-l", "--libpath", dest="library_path", help="Path to folder where credential libs are present")
-	parser.add_option("-f", "--file",  dest="jceks_file_path", help="Path to jceks file to use")
-	parser.add_option("-k", "--key",  dest="key", help="Key to use")
-	parser.add_option("-v", "--value",  dest="value", help="Value to use")
-	parser.add_option("-c", "--create",  dest="create", help="Add a new alias")
-
-	(options, args) = parser.parse_args()
-	library_path = options.library_path
-	jceks_file_path = options.jceks_file_path
-	key = options.key
-	value = options.value
-	getorcreate = 'create' if options.create else 'get'
-	call_keystore(library_path, jceks_file_path, key, value, getorcreate)
+    value = ''
+    if args.create:
+        value = getpass.getpass("Value: ") if sys.stdin.isatty() else sys.stdin.readline().rstrip("\n")
+        getorcreate = 'create'
+    else:
+        getorcreate = 'get'
+    call_keystore(args.library_path, args.jceks_file_path, args.key, value, getorcreate)
 
 
 def call_keystore(libpath, filepath, aliasKey, aliasValue='', getorcreate='get'):
-	finalLibPath = libpath.replace('\\','/').replace('//','/')
-	finalFilePath = 'jceks://file/'+filepath.replace('\\','/').replace('//','/')
-	if getorcreate == 'create':
-		commandtorun = [JAVA_BIN, '-cp', finalLibPath, 'org.apache.ranger.credentialapi.buildks' ,'create', aliasKey, '-value', aliasValue, '-provider',finalFilePath]
-		p = Popen(commandtorun,stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		output, error = p.communicate()
-		statuscode = p.returncode
-		if statuscode == 0:
-			print "Alias " + aliasKey + " created successfully!"
-		else :
-			print "Error creating Alias!! Error: " + str(error)
-		
-	elif getorcreate == 'get':
-		commandtorun = [JAVA_BIN, '-cp', finalLibPath, 'org.apache.ranger.credentialapi.buildks' ,'get', aliasKey, '-provider',finalFilePath]
-		p = Popen(commandtorun,stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		output, error = p.communicate()
-		statuscode = p.returncode
-		if statuscode == 0:
-			print "Alias : " + aliasKey + " Value : " + str(output)
-		else :
-			print "Error getting value!! Error: " + str(error)
-		
-	else:
-		print 'Invalid Arguments!!'
-	
+    finalLibPath = libpath.replace('\\', '/').replace('//', '/')
+    finalFilePath = 'jceks://file/' + filepath.replace('\\', '/').replace('//', '/')
+
+    if getorcreate == 'create':
+        commandtorun = [JAVA_BIN, '-cp', finalLibPath, 'org.apache.ranger.credentialapi.buildks',
+                        'create', aliasKey, '-value', aliasValue, '-provider', finalFilePath]
+        p = Popen(commandtorun, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
+        output, error = p.communicate()
+        if p.returncode == 0:
+            print(f"Alias {aliasKey} created successfully!")
+        else:
+            print(f"Error creating Alias!! Error: {error.strip()}")
+
+    elif getorcreate == 'get':
+        commandtorun = [JAVA_BIN, '-cp', finalLibPath, 'org.apache.ranger.credentialapi.buildks',
+                        'get', aliasKey, '-provider', finalFilePath]
+        p = Popen(commandtorun, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
+        output, error = p.communicate()
+        if p.returncode == 0:
+            print(f"Alias : {aliasKey} Value : {output.strip()}")
+        else:
+            print(f"Error getting value!! Error: {error.strip()}")
+
+    else:
+        print('Invalid Arguments!!')
+
+
 if __name__ == '__main__':
-	main()
+    main()
